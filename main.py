6069921ba740519876d1a4b7703a5d2bcaa546d8
Robert Steinbach <53797736+RobertSteinbach@users.noreply.github.com>
2021-05-10 16:04:55 -0500

    test to see if environment variables set in IDE are published to GIT

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import csv
 from glob import glob
 import os
 
+foo = os.environ.get("FOO")
+print("foo=", foo)
+
+
 app = Flask(__name__)
 stats = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
 keys = ('1', '2', '3', '4', '5', '6', '7', '8', '9')
